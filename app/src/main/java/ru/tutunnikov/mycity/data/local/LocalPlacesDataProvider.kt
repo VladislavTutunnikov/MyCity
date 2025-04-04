7acd58383d2cf2f15adb1c9d6baef31aa840d5f3
VladislavTutunnikov <vladislavtutunnikov77@gmail.com>
2025-04-04 20:29:09 +0300

    add resources

---
+++ app/src/main/java/ru/tutunnikov/mycity/data/local/LocalPlacesDataProvider.kt
@@ -0,0 +1,186 @@
+package ru.tutunnikov.mycity.data.local
+
+import com.yandex.mapkit.geometry.Point
+import ru.tutunnikov.mycity.R
+import ru.tutunnikov.mycity.data.Place
+import ru.tutunnikov.mycity.data.PlaceType
+
+object LocalPlacesDataProvider {
+    val listOfPlaces = listOf(
+        Place(
+            imageId = R.drawable.food_1,
+            name = R.string.food_1,
+            address = R.string.food_1_address,
+            mapCoordinates = Point(48.466963, 38.799980),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_2,
+            name = R.string.food_2,
+            address = R.string.food_2_address,
+            mapCoordinates = Point(48.474543, 38.812817),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_3,
+            name = R.string.food_3,
+            address = R.string.food_3_address,
+            mapCoordinates = Point(48.466118, 38.834053),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_4,
+            name = R.string.food_4,
+            address = R.string.food_4_address,
+            mapCoordinates = Point(48.465856, 38.830315),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_5,
+            name = R.string.food_5,
+            address = R.string.food_5_address,
+            mapCoordinates = Point(48.466173, 38.828502),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_6,
+            name = R.string.food_6,
+            address = R.string.food_6_address,
+            mapCoordinates = Point(48.472874, 38.813635),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_7,
+            name = R.string.food_7,
+            address = R.string.food_7_address,
+            mapCoordinates = Point(48.470599, 38.829221),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_8,
+            name = R.string.food_8,
+            address = R.string.food_8_address,
+            mapCoordinates = Point(48.465683, 38.833874),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_9,
+            name = R.string.food_9,
+            address = R.string.food_9_address,
+            mapCoordinates = Point(48.475445, 38.800501),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.food_10,
+            name = R.string.food_10,
+            address = R.string.food_10_address,
+            mapCoordinates = Point(48.470663, 38.828297),
+            placeType = PlaceType.Food
+        ),
+        Place(
+            imageId = R.drawable.coffee_1,
+            name = R.string.coffee_1,
+            address = R.string.coffee_1_address,
+            mapCoordinates = Point(48.468812, 38.815343),
+            placeType = PlaceType.Coffee
+        ),
+        Place(
+            imageId = R.drawable.coffee_2,
+            name = R.string.coffee_2,
+            address = R.string.coffee_2_address,
+            mapCoordinates = Point(48.470177, 38.808365),
+            placeType = PlaceType.Coffee
+        ),
+        Place(
+            imageId = R.drawable.coffee_3,
+            name = R.string.coffee_3,
+            address = R.string.coffee_3_address,
+            mapCoordinates = Point(48.468982, 38.803361),
+            placeType = PlaceType.Coffee
+        ),
+        Place(
+            imageId = R.drawable.coffee_4,
+            name = R.string.coffee_4,
+            address = R.string.coffee_4_address,
+            mapCoordinates = Point(48.465694, 38.833691),
+            placeType = PlaceType.Coffee
+        ),
+        Place(
+            imageId = R.drawable.coffee_5,
+            name = R.string.coffee_5,
+            address = R.string.coffee_5_address,
+            mapCoordinates = Point(48.469958, 38.802175),
+            placeType = PlaceType.Coffee
+        ),
+        Place(
+            imageId = R.drawable.mall_1,
+            name = R.string.mall_1,
+            address = R.string.mall_1_address,
+            mapCoordinates = Point(48.471439, 38.796347),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_2,
+            name = R.string.mall_2,
+            address = R.string.mall_2_address,
+            mapCoordinates = Point(48.462504, 38.832625),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_3,
+            name = R.string.mall_3,
+            address = R.string.mall_3_address,
+            mapCoordinates = Point(48.465688, 38.831620),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_4,
+            name = R.string.mall_4,
+            address = R.string.mall_4_address,
+            mapCoordinates = Point(48.470729, 38.825316),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_5,
+            name = R.string.mall_5,
+            address = R.string.mall_5_address,
+            mapCoordinates = Point(48.465431, 38.835506),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_6,
+            name = R.string.mall_6,
+            address = R.string.mall_6_address,
+            mapCoordinates = Point(48.464178, 38.832706),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.mall_7,
+            name = R.string.mall_7,
+            address = R.string.mall_7_address,
+            mapCoordinates = Point(48.466542, 38.826090),
+            placeType = PlaceType.Mall
+        ),
+        Place(
+            imageId = R.drawable.gym_1,
+            name = R.string.gym_1,
+            address = R.string.gym_1_address,
+            mapCoordinates = Point(48.469011, 38.800544),
+            placeType = PlaceType.Gym
+        ),
+        Place(
+            imageId = R.drawable.gym_2,
+            name = R.string.gym_2,
+            address = R.string.gym_2_address,
+            mapCoordinates = Point(48.462551, 38.833698),
+            placeType = PlaceType.Gym
+        ),
+        Place(
+            imageId = R.drawable.gym_3,
+            name = R.string.gym_3,
+            address = R.string.gym_3_address,
+            mapCoordinates = Point(48.466114, 38.828886),
+            placeType = PlaceType.Gym
+        )
+    )
+}
